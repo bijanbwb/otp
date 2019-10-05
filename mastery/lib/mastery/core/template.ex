11f3c8538048f480c5c1f60a7b836475242a44de
Bijan <bijanbwb@gmail.com>
2019-10-05 15:29:47 -0400

    Add Quiz functions

--- mastery/lib/mastery/core/template.ex
+++ mastery/lib/mastery/core/template.ex
@@ -4,9 +4,6 @@ defmodule Mastery.Core.Template do
   def new(fields) do
     raw = Keyword.fetch!(fields, :raw)
 
-    struct!(
-      __MODULE__,
-      Keyword.put(fields, :compiled, EEx.compile_string(raw))
-    )
+    struct!(__MODULE__, Keyword.put(fields, :compiled, EEx.compile_string(raw)))
   end
 end
