a9613a2b42e8eb4bf74f86abd9f9a1e080daaa36
Bijan <bijanbwb@gmail.com>
2019-10-14 23:44:25 -0400

    Ecto setup

--- mastery/mastery_persistence/test/mastery_persistence_test.exs
+++ mastery/mastery_persistence/test/mastery_persistence_test.exs
@@ -2,7 +2,7 @@ defmodule MasteryPersistenceTest do
   use ExUnit.Case
   doctest MasteryPersistence
 
-  test "greets the world" do
-    assert MasteryPersistence.hello() == :world
+  test "" do
+    assert 1 + 1 == 2
   end
 end
