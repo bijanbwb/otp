9ee04f67c2ba8dafd04da6236e57dec83bdab1e0
Bijan <bijanbwb@gmail.com>
2019-10-18 15:12:34 -0400

    Generate new FitbOTP mix project
    I resisted the urge to start with Phoenix out of the box so I can
    practice with a base Mix project and simple structs before adding a
    database and all sorts of fanciness.

---
+++ fitbotp/fitbotp/lib/fitbotp.ex
@@ -0,0 +1,18 @@
+defmodule Fitbotp do
+  @moduledoc """
+  Documentation for Fitbotp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fitbotp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ fitbotp/fitbotp/test/fitbotp_test.exs
@@ -0,0 +1,8 @@
+defmodule FitbotpTest do
+  use ExUnit.Case
+  doctest Fitbotp
+
+  test "greets the world" do
+    assert Fitbotp.hello() == :world
+  end
+end
