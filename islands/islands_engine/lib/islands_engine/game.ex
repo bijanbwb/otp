e09bb8c2bd050066f7df804b400add7b02201fcc
Bijan <bijanbwb@gmail.com>
2019-11-01 11:41:31 -0400

    (islands_engine) Game GenServer

--- islands/islands_engine/lib/islands_engine/game.ex
+++ islands/islands_engine/lib/islands_engine/game.ex
@@ -1,28 +1,64 @@
 defmodule IslandsEngine.Game do
-  use GenServer
-
-  @doc """
+  @moduledoc """
 
   ## Example Usage
 
-      iex> {:ok, game} = GenServer.start_link(Game, %{}, [])
+      iex> {:ok, game} = Game.start_link("Bijan")
       {:ok, #PID<0.152.0>}
 
-      iex> GenServer.cast(game, {:message, "Hello"})
+      iex> Game.add_player(game, "Riley")
       :ok
 
-      iex> GenServer.call(game, :message)
-      %{goodbye: "World", hello: "Hello"}
+      iex> state_data = :sys.get_state(game)
+      # ...
+
+      iex> state_data.player2.name
+      "Riley"
   """
-  def init(options \\ %{}) do
-    {:ok, options}
+  use GenServer
+
+  alias IslandsEngine.{Board, Guesses, Rules}
+
+  # API
+
+  def start_link(name) when is_binary(name) do
+    GenServer.start_link(__MODULE__, name, [])
   end
 
-  def handle_cast({:message, value}, state) do
-    {:noreply, Map.put(state, :hello, value)}
+  def add_player(game, name) when is_binary(name) do
+    GenServer.call(game, {:add_player, name})
   end
 
-  def handle_call(:message, _from, state) do
-    {:reply, Map.put(state, :goodbye, "World"), state}
+  defp update_player2_name(state_data, name) do
+    put_in(state_data.player2.name, name)
+  end
+
+  defp update_rules(state_data, rules) do
+    %{state_data | rules: rules}
+  end
+
+  defp reply_success(state_data, reply) do
+    {:reply, reply, state_data}
+  end
+
+  # Callbacks
+
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+    state = %{player1: player1, player2: player2, rules: %Rules{}}
+
+    {:ok, state}
+  end
+
+  def handle_call({:add_player, name}, _from, state_data) do
+    with {:ok, rules} <- Rules.check(state_data.rules, :add_player) do
+      state_data
+      |> update_player2_name(name)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state_data}
+    end
   end
 end
