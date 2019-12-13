223cde6ad271e13b44b4d3487aa75430d34243ee
Bijan <bijanbwb@gmail.com>
2019-12-13 16:09:07 -0500

    (workout) csv importer template

---
+++ workout/workout/lib/workout/csv_importer.ex
@@ -0,0 +1,9 @@
+defmodule Workout.CsvImporter do
+  # File.stream!/1
+  # Stream.map/2
+  # String.replace/2
+  # String.split/2
+  # String.to_integer/1
+  # {{year, month, date}, name}
+  # maps |> list |> Workout.new()
+end
